159b0efcac2f795ece43b219aab3cb95f159ff0a
larryce <larryce@hotmail.com>
2016-10-03 15:41:10 -0300

    questao de string

---
+++ StringProjeto/principal.py
@@ -0,0 +1,9 @@
+import stringutil
+
+texto= input("entre com o texto:")
+letra= input("entre com a letra:")
+stringutil.output(texto)
+stringutil.output(stringutil.tamanho(texto))
+stringutil.output(stringutil.maiusculo(texto))
+stringutil.output(stringutil.minusculo(texto))
+
---
+++ StringProjeto/stringutil.py
@@ -0,0 +1,11 @@
+def output(texto):
+    print("texto:",texto)
+def tamanho(texto):
+    tamanhotexto= len(texto)
+    return tamanhotexto
+def maiusculo(texto):
+    maiusculotexto= texto.upper()
+    return maiusculotexto
+def minusculo(texto):
+    minusculotexto= texto.lower()
+    return minusculotexto
